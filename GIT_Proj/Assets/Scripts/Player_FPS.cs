65f24d8822758297ed4374881a2ec0322ee4cb91
Hideyuki191 <hideyuki1000000@gmail.com>
2020-08-12 11:45:44 +0800

    Updates
    Updated the Stamina bar, now the player won't move when they are out of stamina and will only recover when the player lets go of the shift bar
    
    Also added a slide button, CTRL. Currently is very buggy and only slides in a fixed direction instead of the same direction as where the player is facing
    
    Added a Stamina Bar with the UI to show the player when they are running out of stamina as well as when it is fully recharged

--- GIT_Proj/Assets/Scripts/Player_FPS.cs
+++ GIT_Proj/Assets/Scripts/Player_FPS.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_FPS : MonoBehaviour
 {
@@ -21,9 +22,22 @@ public class Player_FPS : MonoBehaviour
     public float JumpHeight = 1;
     private float gravity = -20;
 
+    #region Slides
+    private bool Sliding = false;
+    private float SlidingTime;
+    private float SlideRotate = 0;
+    private bool SlideFinish = false;
+    private float CurrentYHeight;
+    #endregion
 
-    //Stats
+    #region ExternalStuff
+    public Slider StaminaBar;
+    #endregion
+
+    #region Stats
     private float Stamina = 100;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -36,7 +50,9 @@ public class Player_FPS : MonoBehaviour
     {
         if(GameManager.CurrentState == GameManager.GameState.GameInProgress)
         {
+            SlidingForward();
             DashAndCrash();
+            AllUIBars();
             MouseLook();
             Movement();
             Shoot();
@@ -57,24 +73,28 @@ public class Player_FPS : MonoBehaviour
 
     private void Movement()
     {
+        //Only let the player move themself when they are not sliding, if they are sliding they can't change the slide direction
+        //Apart from the direction they were originally going before they start sliding
+        if(Sliding == false)
+        {
+            //If the player is touching a ground, they will be able to jump and/or move
+            bool OnGround = thisController.isGrounded;
 
-        //If the player is touching a ground, they will be able to jump and/or move
-        bool OnGround = thisController.isGrounded;
+            if (OnGround && playerVelocity.y < 0)
+                playerVelocity.y = 0;
 
-        if (OnGround && playerVelocity.y < 0)
-            playerVelocity.y = 0;
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
 
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+            Vector3 direction = (transform.forward * v + transform.right * h);
+            thisController.Move(direction * Time.deltaTime * MoveSpeed);
 
-        Vector3 direction = (transform.forward * v + transform.right * h);
-        thisController.Move(direction * Time.deltaTime * MoveSpeed);
+            if (Input.GetKeyDown(KeyCode.Space) && OnGround)
+                playerVelocity.y += Mathf.Sqrt(JumpHeight * -3.0f * gravity);
 
-        if (Input.GetKeyDown(KeyCode.Space) && OnGround)
-            playerVelocity.y += Mathf.Sqrt(JumpHeight * -3.0f * gravity);
-
-        playerVelocity.y += gravity * Time.deltaTime;
-        thisController.Move(playerVelocity * Time.deltaTime);
+            playerVelocity.y += gravity * Time.deltaTime;
+            thisController.Move(playerVelocity * Time.deltaTime);
+        }
     }
 
     private void Shoot()
@@ -95,25 +115,105 @@ public class Player_FPS : MonoBehaviour
             if (Stamina >= 30)
             {
                 MoveSpeed = 20;
-                Stamina -= 2 * Time.deltaTime;
+                Stamina -= 10 * Time.deltaTime;
+                
             }
 
-            else if (Stamina < 30)
+            else if (Stamina < 30 && Stamina > 0)
             {
                 MoveSpeed = 5;
+                Stamina -= 10 * Time.deltaTime;
+            }
+
+            else if(Stamina <= 0)
+            {
+                MoveSpeed = 0;
+                Stamina = 0;
             }
         }
 
         else
         {
-            //Else players speed remsins normal and they will gain stamina up to their max stamina
             MoveSpeed = 10;
-            Stamina += 1 * Time.deltaTime;
 
-            if(Stamina >= 100)
+        }
+
+        //If players speed remsins normal and they will gain stamina up to their max stamina
+        if (MoveSpeed == 10)
+        {
+            Stamina += 5 * Time.deltaTime;
+
+            if (Stamina >= 100)
             {
                 Stamina = 100;
             }
         }
+    }
+
+    //Sliding currently incomplete and Buggy
+    private void SlidingForward()
+    {
+        //Set sliding to true when the player presses the left control key
+        //get the current height to use for later codes
+        if(Input.GetKeyDown(KeyCode.LeftControl) && Sliding == false)
+        {
+            CurrentYHeight = gameObject.transform.position.y;
+            Sliding = true;
+        }
+
+        if(Sliding == true)
+        {
+            SlideRotate += 50 * Time.deltaTime;
+
+            SlidingTime += 1 * Time.deltaTime;
+
+            transform.rotation = Quaternion.Euler(SlideRotate, transform.rotation.x, transform.rotation.z);
+            gameObject.transform.position += (-transform.up * 3) * Time.deltaTime;
+
+            //Use the height we Got so that we can keep the players Y value the same as CurrentYHeight
+            //which is it's original Y value so that it doesn't go down below it's original Y value and
+            //glitch through the map
+            if (gameObject.transform.position.y <= CurrentYHeight)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x , CurrentYHeight);
+            }
+
+        }
+
+        //Slide Duration, a.k.a how long the character will slide for
+        if (SlidingTime >= 3)
+        {
+            SlideRotate -= 100 * Time.deltaTime;
+            SlidingTime = 0;
+            Sliding = false;
+            SlideFinish = true;
+        }
+
+        //On slide finish, stop sliding and if rotation is greater than 0 (From sliding rotation would be 90)
+        //Set the rotation back to 0
+        if(Sliding == false && SlideRotate >= 0 && SlideFinish == true)
+        {
+            transform.rotation = Quaternion.Euler(SlideRotate, transform.rotation.x, transform.rotation.z);
+            SlideFinish = false;
+        }
+        //Don't let the rotation be greater than 90
+        if(SlideRotate >= 90)
+        {
+            SlideRotate = 90;
+        }
+
+        //Don't let the rotation be lesser than 0
+        else if(SlideRotate <= 0)
+        {
+            SlideRotate = 0;
+        }
+
+        
+    }
+
+    private void AllUIBars()
+    {
+        StaminaBar.maxValue = 100;
+        StaminaBar.value = Stamina;
     }
 }
