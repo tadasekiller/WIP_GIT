c0feae10210d6a20a4875ddea196c2e8616bb851
Hideyuki191 <hideyuki1000000@gmail.com>
2020-08-18 12:22:25 +0800

    Sliding fixed (Mostly)
    Everything works now except for when you're moving while sliding your x and z positions may change to a different location from where you were when you were sliding.

--- GIT_Proj/Assets/Scripts/Player_FPS.cs
+++ GIT_Proj/Assets/Scripts/Player_FPS.cs
@@ -8,6 +8,7 @@ public class Player_FPS : MonoBehaviour
     public static Player_FPS thisPlayer;
     public float MouseSensitivity = 1;
     public Transform FPSCamera;
+    public GameObject CameraCameraCamera;
     private float RotateY = 0;
     private CharacterController thisController = null;
     
@@ -71,21 +72,26 @@ public class Player_FPS : MonoBehaviour
 
     private void MouseLook()
     {
+        if(Sliding == false)
+        {
+            //Lets the player move the camera with their mouse
+            float rotateX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * MouseSensitivity;
 
-        //Lets the player move the camera with their mouse
-        float rotateX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * MouseSensitivity;
-
-        RotateY -= Input.GetAxis("Mouse Y") * MouseSensitivity;
-        RotateY = Mathf.Clamp(RotateY, -60, 60);
-        FPSCamera.localEulerAngles = new Vector3( RotateY,  0, 0);
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, rotateX, transform.localEulerAngles.z);
+            RotateY -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+            RotateY = Mathf.Clamp(RotateY, -60, 60);
+            FPSCamera.localEulerAngles = new Vector3(RotateY, 0, 0);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, rotateX, transform.localEulerAngles.z);
+        }
     }
 
     private void Movement()
     {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
         //Only let the player move themself when they are not sliding, if they are sliding they can't change the slide direction
         //Apart from the direction they were originally going before they start sliding
-        if(Sliding == false)
+        if (Sliding == false)
         {
             //If the player is touching a ground, they will be able to jump and/or move
             bool OnGround = thisController.isGrounded;
@@ -102,10 +108,6 @@ public class Player_FPS : MonoBehaviour
                 thisController.slopeLimit = 45;
             }
 
-
-            float h = Input.GetAxisRaw("Horizontal");
-            float v = Input.GetAxisRaw("Vertical");
-
             Vector3 direction = (transform.forward * v + transform.right * h);
             thisController.Move(direction * Time.deltaTime * MoveSpeed);
             //Jump
@@ -117,6 +119,12 @@ public class Player_FPS : MonoBehaviour
 
             playerVelocity.y += gravity * Time.deltaTime;
             thisController.Move(playerVelocity * Time.deltaTime);
+        }
+
+        if(Sliding == true)
+        {
+            Vector3 direction = (-transform.up * v + -transform.right * h);
+            thisController.Move(direction * Time.deltaTime * MoveSpeed);
         }
     }
 
@@ -176,13 +184,16 @@ public class Player_FPS : MonoBehaviour
 
         if(Sliding == true)
         {
+            float rotateX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * MouseSensitivity;
+            RotateY = Mathf.Clamp(RotateY, -100, 100);
+
             SlideRotate += 50 * Time.deltaTime;
 
             SlidingTime += 1 * Time.deltaTime;
 
-            transform.rotation = Quaternion.Euler(SlideRotate, transform.rotation.x, transform.rotation.z);
-            gameObject.transform.position += (-transform.up * 3) * Time.deltaTime;
-
+            transform.rotation = Quaternion.Euler(-SlideRotate, transform.rotation.y, transform.rotation.z);
+            FPSCamera.localEulerAngles = new Vector3(RotateY, 0, 0);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, rotateX, transform.localEulerAngles.z);
             //Use the height we Got so that we can keep the players Y value the same as CurrentYHeight
             //which is it's original Y value so that it doesn't go down below it's original Y value and
             //glitch through the map
@@ -191,12 +202,16 @@ public class Player_FPS : MonoBehaviour
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x , CurrentYHeight);
             }
 
+            else if(gameObject.transform.position.y >= CurrentYHeight)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, CurrentYHeight);
+            }
+
         }
 
         //Slide Duration, a.k.a how long the character will slide for
-        if (SlidingTime >= 3)
+        if (SlidingTime >= 5)
         {
-            SlideRotate -= 100 * Time.deltaTime;
             SlidingTime = 0;
             Sliding = false;
             SlideFinish = true;
@@ -204,22 +219,20 @@ public class Player_FPS : MonoBehaviour
 
         //On slide finish, stop sliding and if rotation is greater than 0 (From sliding rotation would be 90)
         //Set the rotation back to 0
-        if(Sliding == false && SlideRotate >= 0 && SlideFinish == true)
+        if(SlideFinish == true)
         {
-            transform.rotation = Quaternion.Euler(SlideRotate, transform.rotation.x, transform.rotation.z);
+            float rotateX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * MouseSensitivity;
+            transform.rotation = Quaternion.Euler(0, rotateX, transform.rotation.z);
             SlideFinish = false;
+            SlideRotate = 0;
         }
         //Don't let the rotation be greater than 90
-        if(SlideRotate >= 90)
+        if(SlideRotate >= 85)
         {
-            SlideRotate = 90;
+            SlideRotate = 85;
         }
 
         //Don't let the rotation be lesser than 0
-        else if(SlideRotate <= 0)
-        {
-            SlideRotate = 0;
-        }
 
         
     }
