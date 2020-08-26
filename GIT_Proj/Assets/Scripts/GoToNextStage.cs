22c57961e7360f95c49eebf66312b7623d9a60e9
Hideyuki191 <hideyuki1000000@gmail.com>
2020-08-26 12:15:56 +0800

    Script
    Added a goal in stage 1 and a script that brings player from stage 1 to stage 2

---
+++ GIT_Proj/Assets/Scripts/GoToNextStage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextStage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if(Other.tag == "Player")
+        {
+            //Name of Stage 2
+            SceneManager.LoadScene("");
+        }
+    }
+}
